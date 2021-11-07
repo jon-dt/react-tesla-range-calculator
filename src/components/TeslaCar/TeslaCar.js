3e5408bcfbdbc7c67b939903ac81e02e16e6caae
Jonathan T <64747845+jon-dt@users.noreply.github.com>
2021-11-07 05:30:26 -0600

    Lesson 1.10 Updates up to lesson 1.10.
    Im implemented an provitional solution to load the images in the layout. Im loading the images from the internet, not yet from codesandbox local storage

--- src/components/TeslaCar/TeslaCar.js
+++ src/components/TeslaCar/TeslaCar.js
@@ -1,21 +1,15 @@
 import "./TeslaCar.css";
 import PropTypes from "react";
-import Car from "../../assets/tesla.jpg";
 
 const TeslaCar = (props) => (
   <div className="tesla-car">
-    <img src={Car} alt="Car" />
     <div className="tesla-wheels">
       <div
         className={`tesla-wheel tesla-wheel--front tesla-wheel--${props.wheelsize}`}
-      >
-        sdasdsadad
-      </div>
+      ></div>
       <div
         className={`tesla-wheel tesla-wheel--rear tesla-wheel--${props.wheelsize}`}
-      >
-        sdasdsadad
-      </div>
+      ></div>
     </div>
   </div>
 );
